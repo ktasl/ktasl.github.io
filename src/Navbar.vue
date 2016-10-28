bf81e101303910261a7d17ec346b7887e856cd61
michaelangelo <michaelangelo@cht.com.tw>
2016-10-28 17:12:13 +0800

    ### 0.0.2 ``` 完成用Vue建照三個欄位，並且使用 Css完成初步navbar ``` 真麻煩，又不小心在master上面工作了...

---
+++ src/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+	<!-- nav bar menu -->
+	<div id='navbar' class="ui fixed inverted menu">
+		<div class="ui secondary menu">
+			<a href="#" class="header item">
+				<img class="logo" src="./assets/img/logo.png"> Project Name
+			</a>
+			<a href="#" class="item">{{ home }}</a>
+			<div class="ui simple dropdown item">
+				{{ dropdown }} <i class="dropdown icon"></i>
+				<div class="menu">
+					<a class="item" href="#">Link Item</a>
+					<a class="item" href="#">Link Item</a>
+					<div class="divider"></div>
+					<div class="header">Header Item</div>
+					<div class="item">
+						<i class="dropdown icon"></i> Sub Menu
+						<div class="menu">
+							<a class="item" href="#">Link Item</a>
+							<a class="item" href="#">Link Item</a>
+						</div>
+					</div>
+					<a class="item" href="#">Link Item</a>
+				</div>
+			</div>
+
+		</div>
+		<div class="right menu">
+			<div class="ui primary button">{{ singup }}</div>
+		</div>
+	</div>
+</template>
+
+<script>
+    export default {
+        name: 'navbar',
+        data() {
+            return {
+                home: '專案',
+                dropdown: '下拉式選單',
+                singup: `登入`
+            }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
+import './assets/css/menu.css'
+import './assets/css/dropdown.css'
+import './assets/css/button.css'
+import './assets/css/popup.css'
+
 import Vue from 'vue'
-import App from './App.vue'
+import Navbar from './Navbar.vue'
+import MainContainer from './MainContainer.vue'
+import Footer from './Footer.vue'
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#navbar',
+    render: h => h(Navbar)
+})
+
+new Vue({
+    el: '#maincontainer',
+    render: h => h(MainContainer)
+})
+
+new Vue({
+    el: '#footer',
+    render: h => h(Footer)
 })
